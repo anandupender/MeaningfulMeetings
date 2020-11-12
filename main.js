29160aaf134a87d1162e45a11da5857367b3805c
anandupender <anand.upender@gmail.com>
2020-11-12 11:40:09 -0500

    small change

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed;
-var questions = ["Ask Alan how the internet works.", "Ask Renae how to write a proper headline.", "Ask Tessa about the most interesting TED event she's helped create.", "Ask Yael about her classes on social justice", "Ask Alan about his most recent Damn Interesting episode", "Ask Connie about her PhD thesis.", "Ask Renae about the most bizarre story she has written", "Ask Tessa about the most Canadian part of her personality/life", "Ask Alan about his favorite place in Utah","Ask Justin about his favorite Roman emperor", " Ask Anand about his dream design project", "Ask Connie about her favorite online conspiracy theory","Ask Justin what he'd be doing if he hadn't gone into tech","Ask Yael about the last book she's read","Ask Alan about his thoughts on TikTok", "Ask Tessa about her favorite vacation spot","Ask Anand about his cast iron skillet baby", "Ask Connie about her perfect day", "Ask everyone to share the cutest photo of their child or pet"];
+var questions = ["Ask Alan how the internet works.", "Ask Renae how to write a proper headline.", "Ask Tessa about the most interesting TED event she's helped create.", "Ask Yael about her classes on social justice", "Ask Alan about his most recent Damn Interesting episode", "Ask Connie about her PhD thesis.", "Ask Renae about the most bizarre story she has written", "Ask Tessa about the most Canadian part of her personality/life", "Ask Alan about his favorite place in Utah","Ask Justin about his favorite Roman emperor", "Ask Connie about her favorite online conspiracy theory","Ask Justin what he'd be doing if he hadn't gone into tech","Ask Yael about the last book she's read","Ask Alan about his thoughts on TikTok", "Ask Tessa about her favorite vacation spot","Ask Anand about his cast iron skillet baby", "Ask Connie about her perfect day", "Ask everyone to share the cutest photo of their child or pet"];
 
 function checkTime(){
     let date = new Date();  
@@ -43,12 +43,12 @@ function startKeyboard(){
 
             // show a cool gif
             case "KeyG":
-                window.open("https://giphy.com/search/cat");
+                window.open("https://giphy.com/explore/random");
                 break;
 
             // show random TED talk 
             case "KeyT":
-                window.open("https://www.ted.com/talks/angela_koine_flynn_the_science_of_skin_color");
+                window.open("https://www.youtube.com/teded/videos?view=0&sort=p&flow=grid");
                 break;
         }
     })
